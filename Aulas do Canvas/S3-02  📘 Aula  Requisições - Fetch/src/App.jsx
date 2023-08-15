9bb514dc46fa83f64a7e20c951244fd5cb00c0ab
Alexsander Sampaio Lima <alexsandersampaiolima12@gmail.com>
2023-08-15 18:53:53 -0300

    Aula Axios

--- Aulas do Canvas/S3-02  📘 Aula  Requisições - Fetch/src/App.jsx
+++ Aulas do Canvas/S3-02  📘 Aula  Requisições - Fetch/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { api } from "./services/api"
 function App() {
 
-  const getFruits = async () => {
-    // const response = await fetch("/fruits");
-    // const json = await response.json();
+  const [category, setCategory] = useState("");
+  const [fruits, setFruits] = useState([]);
 
-    const { data } = await api.get("fruits")
-    console.log(data)
-    setFruits(data);
-  }
+  useEffect(() => {
+    const getFruits = async () => {
+      const { data } = await api.get("fruits", {
+        params: {
+          category: category !== "" ? category : undefined,
+        }
+      })
+      setFruits(data);
+    }
+    getFruits()
+  }, [category])
 
   const removeListFruits = () => {
     setFruits([])
   }
 
-  const [fruits, setFruits] = useState([]);
-
   return (
     <>
-      <button onClick={getFruits}>Requisições</button>
-      <button onClick={removeListFruits}>fechar</button>
-      {fruits.map(fruits => (
-        <li key={fruits.id}>
-          <h1>{`Nome da fruta: ${fruits.name}`}</h1>
-          <h2>{`Tipo de fruta: ${fruits.category}`}</h2>
-          <h3>{`Preço da fruta: ${fruits.price}`}</h3>
+      <ul>
+        <li>
+          <button onClick={() => setCategory("")}>Todos</button>
         </li>
-      ))}
+        <li>
+          <button onClick={() => setCategory("vermelha")}>Vermelha</button>
+        </li>
+        <li>
+          <button onClick={() => setCategory("amarela")}>Amarelas</button>
+        </li>
+      </ul>
+      <ul>
+        {fruits.map(fruits => (
+          <li key={fruits.id}>
+            <h1>{`Nome da fruta: ${fruits.name}`}</h1>
+            <p>{`Tipo de fruta: ${fruits.category}`}</p>
+            <p>{`Preço da fruta: ${fruits.price}`}</p>
+          </li>
+        ))}
+        <button onClick={removeListFruits}>fechar</button>
+      </ul>
     </>
   )
 }
