e8c79cd5b519d41f2239c71fd21ab090e42f040a
Alexsander Sampaio Lima <alexsandersampaiolima12@gmail.com>
2023-08-15 18:40:44 -0300

    Aula sobre axios

--- Aulas do Canvas/S3-02  📘 Aula  Requisições - Fetch/src/App.jsx
+++ Aulas do Canvas/S3-02  📘 Aula  Requisições - Fetch/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react"
-
+import { api } from "./services/api"
 function App() {
 
   const getFruits = async () => {
-    const response = await fetch("https://fruit-fake-api.onrender.com/fruits");
-    const json = await response.json();
-    setFruits(json);
+    // const response = await fetch("/fruits");
+    // const json = await response.json();
+
+    const { data } = await api.get("fruits")
+    console.log(data)
+    setFruits(data);
+  }
+
+  const removeListFruits = () => {
+    setFruits([])
   }
 
   const [fruits, setFruits] = useState([]);
@@ -13,13 +20,14 @@ function App() {
   return (
     <>
       <button onClick={getFruits}>Requisições</button>
-      {fruits.map(fruits =>(
+      <button onClick={removeListFruits}>fechar</button>
+      {fruits.map(fruits => (
         <li key={fruits.id}>
-          {fruits.name}
-          {fruits.price}
-          {fruits.category}
+          <h1>{`Nome da fruta: ${fruits.name}`}</h1>
+          <h2>{`Tipo de fruta: ${fruits.category}`}</h2>
+          <h3>{`Preço da fruta: ${fruits.price}`}</h3>
         </li>
-      ) )}
+      ))}
     </>
   )
 }
