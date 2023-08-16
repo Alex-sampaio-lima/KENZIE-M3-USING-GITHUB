d934167bf00789039bae2170033f1aadf2de2df7
Alexsander Sampaio <alexsandersampaiolima12@gmail.com>
2023-08-15 22:48:34 -0300

    teste

--- Aulas do Canvas/S3-02  📘 Aula  Requisições - Fetch/src/App.jsx
+++ Aulas do Canvas/S3-02  📘 Aula  Requisições - Fetch/src/App.jsx
@@ -2,28 +2,71 @@ import { useEffect, useState } from "react"
 import { api } from "./services/api"
 function App() {
 
+  // useEffect(() => {
+  //   const getFruits = async () => {
+  //     const { data } = await api.get("fruits", {
+  //       params: {
+  //         category: category !== "" ? category : undefined,
+  //       }
+  //     })
+  //     setFruits(data);
+  //   }
+  //   getFruits()
+  // }, [category])
+
+  // const removeListFruits = () => {
+  //   setFruits([])
+  // }
+
   const [category, setCategory] = useState("");
   const [fruits, setFruits] = useState([]);
+  const [valueCircle, setvalueCircle] = useState(5)
+  const [total, setTotal] = useState([])
 
   useEffect(() => {
-    const getFruits = async () => {
-      const { data } = await api.get("fruits", {
-        params: {
-          category: category !== "" ? category : undefined,
-        }
-      })
-      setFruits(data);
+    const sumCircle = () => {
+      let soma 
+      let pi = 3.14
+      soma = pi * (valueCircle ** 2)
+      setvalueCircle(valueCircle)
+      setTotal([...total, soma])
+      console.log(pi)
+      console.log(total)
+      console.log(soma)
     }
-    getFruits()
-  }, [category])
+    sumCircle()
+  }, [valueCircle])
 
-  const removeListFruits = () => {
-    setFruits([])
-  }
+  useEffect(() => {
+    const remove = () => {
+      if (setTotal == 0) {
+        setvalueCircle()
+        setTotal([])
+      }
+    }
+    remove()
+  }, [valueCircle])
+
+
+
 
   return (
     <>
       <ul>
+        <input type="number"
+          value={valueCircle}
+          placeholder="Digite um número"
+          onChange={(event) => setvalueCircle(event.target.value)} />
+      </ul>
+      <button>Limpar</button>
+      <h1>{valueCircle}</h1>
+      <ul>
+        <h1>{total}</h1>
+      </ul>
+
+
+      {/* <h1>{total}</h1> */}
+      {/* <ul>
         <li>
           <button onClick={() => setCategory("")}>Todos</button>
         </li>
@@ -43,7 +86,7 @@ function App() {
           </li>
         ))}
         <button onClick={removeListFruits}>fechar</button>
-      </ul>
+      </ul> */}
     </>
   )
 }
